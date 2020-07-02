efd6fd59026bd5c0084a8ac3c6da120ff61fd2ed
PDT <darkflayer@gmail.com>
2020-07-02 18:05:27 +0200

    added update asset type to asset type manager

--- src/asset/asset_type_manager.py
+++ src/asset/asset_type_manager.py
@@ -10,6 +10,7 @@ from database import Column, DataTypes
 from database.db_connection import DbConnection
 from database.sqlite_connection import SqliteConnection
 from asset import AssetType
+from exceptions.asset import AssetTypeAlreadyExistsException, AssetTypeDoesNotExistException
 
 
 class AssetTypeManager:
@@ -29,28 +30,24 @@ class AssetTypeManager:
         # Ensuring the table to store the asset types in exists
         self._init_asset_types_table()
 
+        # Making sure one can't create more than one asset type with the same name
         if self.check_asset_type_exists(asset_type):
-            self.db_connection.reset()
+            raise AssetTypeAlreadyExistsException(f"The asset type {asset_type.asset_name} already exists!")
 
         # Creating a query dict as required by write_dict
+        asset_table_name = AssetTypeManager.generate_asset_table_name(asset_type)
         query_dict = {
             'primary_key': asset_type.asset_type_id,
             'asset_name': asset_type.asset_name,
-            'asset_table_name': AssetTypeManager.generate_asset_table_name(asset_type),
-            'asset_columns': ' '.join([
-                f"{column.name} {column.datatype.db_type} {int(column.required)}"
-                for column in asset_type.columns
-            ])
+            'asset_table_name': asset_table_name,
+            'asset_columns': self.generate_str_column_from_columns(asset_type.columns)
         }
 
         # Storing the type information in the appropriate table
         self.db_connection.write_dict(self._asset_types_table_name, query_dict)
 
         # Creating a table appropriate for the asset_type
-        self.db_connection.create_table(
-            AssetTypeManager.generate_asset_table_name(asset_type),
-            asset_type.columns
-        )
+        self.db_connection.create_table(asset_table_name, asset_type.columns)
         self.db_connection.commit()
 
     def delete_asset_type(self, asset_type: AssetType):
@@ -62,18 +59,49 @@ class AssetTypeManager:
 
     def update_asset_type(self, asset_type: AssetType):
         """Update an ``asset_type`` in the database."""
-        # TODO
-        pass
+
+        # Making sure one is not trying to update an asset type without an id
+        if not asset_type.asset_type_id:
+            raise AttributeError("The asset_type_id parameter of an asset type you are trying to update must exist!")
+
+        # Ensuring the table, to store the asset types in, exists
+        self._init_asset_types_table()
+
+        # getting the old asset type from the database
+        db_asset_type = self.get_one(asset_type.asset_type_id)
+
+        updated_table_name = self.generate_asset_table_name(asset_type)
+        # Updating the "abasset.." tables name
+        if db_asset_type.asset_name != asset_type.asset_name:
+
+            # Making sure one can't update to a name that already exists
+            if self.check_asset_type_exists(asset_type):
+                raise AssetTypeAlreadyExistsException("Can't perform update - AssetType with that name already exists!")
+
+            self.db_connection.update_table_name(
+                db_asset_type.asset_table_name,
+                updated_table_name
+            )
+
+        # Updating the "abasset.." tables columns
+        self.db_connection.update_table_columns(updated_table_name, asset_type.columns)
+
+        # Creating a query dict as required by update
+        values = {
+            'primary_key': asset_type.asset_type_id,
+            'asset_name': asset_type.asset_name,
+            'asset_table_name': updated_table_name,
+            'asset_columns': self.generate_str_column_from_columns(asset_type.columns)
+        }
+        self.db_connection.update(self._asset_types_table_name, values)
 
     def check_asset_type_exists(self, asset_type: AssetType) -> bool:
-        """Check if ``asset_type`` exists."""
+        """Check if ``asset_type`` with that name already exists."""
 
         db_response = self.db_connection.read(
             table_name=self._asset_types_table_name,
             headers=['primary_key', 'asset_name'],
-            or_filters=[
-                f"asset_name = '{asset_type.asset_name}'"
-            ]
+            or_filters=[f"asset_name = '{asset_type.asset_name}'"]
         )
 
         table_exists = self.db_connection.check_table_exists(
@@ -94,7 +122,7 @@ class AssetTypeManager:
 
         assets_types = []
         for asset_type_row in result:
-            assets_types.append(self.get_asset_type_from_str(
+            assets_types.append(self.generate_asset_type_from_str(
                 asset_name=asset_type_row['asset_name'],
                 asset_columns=asset_type_row['asset_columns'],
                 asset_table_name=asset_type_row['asset_table_name'],
@@ -119,7 +147,7 @@ class AssetTypeManager:
         if len(result) < 1:
             return None
 
-        asset_type = self.get_asset_type_from_str(
+        asset_type = self.generate_asset_type_from_str(
             asset_name=result[0]['asset_name'],
             asset_columns=result[0]['asset_columns'],
             asset_table_name=result[0]['asset_table_name'],
@@ -133,7 +161,7 @@ class AssetTypeManager:
     ######################
 
     @staticmethod
-    def get_asset_type_from_str(
+    def generate_asset_type_from_str(
             asset_type_id: int,
             asset_name: str,
             asset_table_name: str,
@@ -154,6 +182,11 @@ class AssetTypeManager:
                 ]
             ]
         )
+
+    @staticmethod
+    def generate_str_column_from_columns(columns: Sequence[Column]):
+        """Generate a column str from a list of Columns."""
+        return ' '.join([f"{column.name} {column.datatype.db_type} {int(column.required)}" for column in columns])
 
     @staticmethod
     def generate_asset_table_name(asset_type: AssetType) -> str:
