7dbee9f9f7036d4a7e0b454e6eb62ff518adb9a4
PDT <darkflayer@gmail.com>
2020-07-02 18:05:00 +0200

    made column a dataclass added .get methode to DataTypes

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -4,9 +4,11 @@
 
 This is the database package.
 """
-
+from dataclasses import dataclass
 from datetime import datetime
 from typing import NamedTuple
+
+from exceptions.database import DataTypeDoesNotExistException
 
 
 class DataType(NamedTuple):
@@ -25,11 +27,20 @@ class DataTypes:
     REAL = NUMBER
     INTEGER = DataType(typename='INTEGER', db_type='INTEGER', convert=int)
     DATETIME = DataType(typename='DATETIME', db_type='INTEGER', convert=datetime.utcfromtimestamp)
+
     # TODO: Add additional required types
     # TODO: Update sqlite connection using this
 
+    @staticmethod
+    def get(type_name: str):
+        data_type = DataTypes.__dict__.get(type_name, None)
+        if data_type:
+            return data_type
+        raise DataTypeDoesNotExistException(f"The datatype {type_name} does not exist!")
 
-class Column(NamedTuple):
+
+@dataclass
+class Column:
     """This is a column, as required to create database column."""
     name: str
     datatype: DataType
