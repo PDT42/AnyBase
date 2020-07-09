b07456259d722737485d10e3598c24f6809b3440
PDT <darkflayer@gmail.com>
2020-07-09 21:54:37 +0200

    removed redundant thread lock

--- src/server/asset_server.py
+++ src/server/asset_server.py
@@ -16,35 +16,32 @@ from asset.asset_type_manager import AssetTypeManager
 def create_asset(asset_type_id: int):
     """This is a FlaskAppRoute that lets you create an ``Asset``."""
 
-    thread_lock = Lock()
+    asset_type_manager = AssetTypeManager()
+    asset_type = asset_type_manager.get_one(asset_type_id)
 
-    with thread_lock:
-        asset_type_manager = AssetTypeManager()
-        asset_type = asset_type_manager.get_one(asset_type_id)
+    if request.method == 'POST':
 
-        if request.method == 'POST':
+        asset_manager = AssetManager()
 
-            asset_manager = AssetManager()
+        asset_data = {}
 
-            asset_data = {}
+        for column in asset_type.columns:
 
-            for column in asset_type.columns:
+            # Column is missing in form but is required
+            if column.required and column.name not in request.form.keys():
+                return 1  # TODO: Validation
 
-                # Column is missing in form but is required
-                if column.required and column.name not in request.form.keys():
-                    return 1  # TODO: Validation
+            # Column is missing and isn't required
+            elif not column.required and column.name not in request.form.keys():
+                continue
 
-                # Column is missing and isn't required
-                elif not column.required and column.name not in request.form.keys():
-                    continue
+            # Column is present
+            else:
+                asset_data[column.name] = column.datatype.convert(request.form[column.name])
 
-                # Column is present
-                else:
-                    asset_data[column.name] = column.datatype.convert(request.form[column.name])
+        asset = Asset(asset_id=None, data=asset_data)
+        asset_manager.create_asset(asset_type, asset)
 
-            asset = Asset(asset_id=None, data=asset_data)
-            asset_manager.create_asset(asset_type, asset)
-
-            return redirect(f'/asset-type/{asset_type_id}')
+        return redirect(f'/asset-type/{asset_type_id}')
 
     return render_template("create-asset.html", asset_type=asset_type)
