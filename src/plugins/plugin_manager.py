04f90decab9197bd4ca075fba13c776f9dde6110
PDT <darkflayer@gmail.com>
2020-07-20 22:30:46 +0200

    initialized plugin_manager

---
+++ src/plugins/plugin_manager.py
@@ -0,0 +1,8 @@
+"""
+:Author: PDT
+:Since: 2020/07/19
+
+This is the plugin manager. As the name suggests it is the connection between the backend
+and the database and provides all required functionalities for interacting plugin settings
+stored in the database.
+"""
