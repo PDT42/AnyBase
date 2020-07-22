9fd3c6d501ca3876e9344890f6b3206e70b26dbc
PDT <darkflayer@gmail.com>
2020-07-22 22:46:37 +0200

    Started implementing PluginSettingsManager

--- src/plugins/plugin_settings_manager.py
+++ src/plugins/plugin_settings_manager.py
@@ -2,26 +2,30 @@
 :Author: PDT
 :Since: 2020/07/19
 
-This is the plugin manager. As the name suggests it is the connection between the backend
-and the database and provides all required functionalities for interacting plugin settings
-stored in the database.
+This is the plugin settings manager. As the name suggests it is the connection between the
+backend and the database and provides all required functionalities for interacting plugin
+settings stored in the database.
 """
-from typing import Sequence
 
-from database import Column
+from typing import Any, Mapping, Sequence
+
+from database import Column, DataTypes
 from database.db_connection import DbConnection
 from database.sqlite_connection import SqliteConnection
 from plugins import PluginSettings
+from plugins.plugin_manager import PluginManager
 
 
-class PluginManager:
-    """This is the ``PluginManager``."""
+class PluginSettingsManager:
+    """This is the ``PluginSettingsManager``."""
 
     def __init__(self):
-        """Create a new ``PluginManager``."""
+        """Create a new ``PluginSettingsManager``."""
 
-        self._plugin_headers = []
-        self._plugin_table_name = 'abintern_plugin_settings'
+        self._plugin_settings_headers = ['plugin', 'employed_columns']
+        self._plugin_settings_table_name = 'abintern_plugin_settings'
+
+        self.plugin_manager = PluginManager()
 
         self.db_connection: DbConnection = SqliteConnection.get()
 
@@ -31,11 +35,33 @@ class PluginManager:
         # Ensuring the table to store the plugin settings in exists
         self._init_plugin_settings_table()
 
+    def get_plugin_settings(self, plugin_settings_id: int):
+        """Get ``PluginSettings`` from the database."""
+
+        result: Sequence[Mapping[str, Any]] = self.db_connection.read(
+            table_name=self._plugin_settings_table_name,
+            headers=self._plugin_settings_headers,
+            and_filters=[f'primary_key = {plugin_settings_id}']
+        )
+
+        if len(result) < 1:
+            return None
+
+        plugin_settings = PluginSettings(
+
+        )
+
+    def _generate_employed_columns_string(self, columns: Sequence[Column]):
+        """Generate a string to use in 'employed_columns'."""
+        pass
+
     def _init_plugin_settings_table(self):
         """Initialize the plugins settings table."""
 
         plugin_settings_columns: Sequence[Column] = [
-            Column(),
+            # The column 'primary_key' will be created automatically
+            Column('plugin', 'plugin', DataTypes.INTEGER.value, required=True),
+            Column('employed_columns', 'employed_columns', DataTypes.VARCHAR.value, required=True)
         ]
 
-        self.db_connection.create_table(self._plugin_table_name)
+        self.db_connection.create_table(self._plugin_settings_table_name, plugin_settings_columns)
