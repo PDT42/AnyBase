5568138028f8cdf8da2f8a691dfea811f484fac4
PDT <darkflayer@gmail.com>
2020-07-22 14:29:05 +0200

    Added routing for configuration page

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -7,7 +7,18 @@ These are the routes for the project.
 
 from flask import render_template
 
+from asset.asset_type_manager import AssetTypeManager
+
 
 def index():
     """Return home page."""
     return render_template("base.html")
+
+
+def configuration():
+    """Return Configuration page."""
+
+    asset_type_manager = AssetTypeManager()
+    asset_types = asset_type_manager.get_all()
+
+    return render_template("configuration.html", asset_types=asset_types)
