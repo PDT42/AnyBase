537d9092465ea1590da09270bb9b973852d9c5f7
PDT <darkflayer@gmail.com>
2020-07-25 23:14:44 +0200

    Added id parameters to plugin types

--- src/plugins/__init__.py
+++ src/plugins/__init__.py
@@ -5,7 +5,7 @@
 This is the module for the plugins, the functional components of the software.
 """
 from dataclasses import dataclass
-from typing import Any, Mapping
+from typing import Sequence
 
 
 @dataclass
@@ -13,10 +13,12 @@ class Plugin:
     """This is a plugin."""
     plugin_name: str
     plugin_macro_path: str
+    plugin_id: int = None
 
 
 @dataclass
 class PluginSettings:
     """This is as set of plugin settings."""
     plugin: Plugin
-    employed_columns: Mapping[Any, str]
+    employed_columns: Sequence[str]
+    plugin_settings_id: int = None
