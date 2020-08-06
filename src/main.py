85e807df0509cb8c0914468e8a002aeec686079f
PDT <darkflayer@gmail.com>
2020-08-06 09:23:44 +0200

    made asset type server a singleton

--- src/main.py
+++ src/main.py
@@ -5,10 +5,10 @@
 
 from flask import Flask
 
-from server import index, configuration
 from config import Config
+from server import configuration, index
 from server.asset_server import create_asset
-from server.asset_type_server import delete_asset_type, get_one_asset_type, get_all_asset_types, create_asset_type
+from server.asset_type_server import AssetTypeServer
 
 # Getting config values
 # ---------------------
@@ -18,16 +18,47 @@ template_folder = Config.get().read('frontend', 'template_folder', '/res/templat
 # --------------------------
 app = Flask(__name__, template_folder=template_folder)
 
+# Creating Servers
+# ----------------
+asset_type_server = AssetTypeServer.get()
+
 # Adding Routes provided in server to app
 # ---------------------------------------
+
+# General Routes
+# ~~~~~~~~~~~~~~
 app.add_url_rule('/', 'index', index, methods=['GET'])
 app.add_url_rule('/configuration', 'configuration', configuration, methods=['GET'])
 
-app.add_url_rule('/asset-type/create', 'create-asset-type', create_asset_type, methods=['GET', 'POST'])
-app.add_url_rule('/asset-types', 'asset-types', get_all_asset_types, methods=['GET'])
-app.add_url_rule('/asset-type/<int:asset_type_id>', 'asset-type', get_one_asset_type, methods=['GET'])
-app.add_url_rule('/asset-type/<int:asset_type_id>', 'delete-asset-type', delete_asset_type, methods=['POST'])
+# AssetType Routes
+# ~~~~~~~~~~~~~~~~
+app.add_url_rule(
+    '/asset-type/create',
+    'create-asset-type',
+    asset_type_server.create_asset_type,
+    methods=['GET', 'POST']
+)
+app.add_url_rule(
+    '/asset-types',
+    'asset-types',
+    asset_type_server.get_all_asset_types,
+    methods=['GET']
+)
+app.add_url_rule(
+    '/asset-type/<int:asset_type_id>',
+    'asset-type',
+    asset_type_server.get_one_asset_type,
+    methods=['GET']
+)
+app.add_url_rule(
+    '/asset-type/<int:asset_type_id>',
+    'delete-asset-type',
+    asset_type_server.delete_asset_type,
+    methods=['POST']
+)
 
+# Asset Routes
+# ~~~~~~~~~~~~
 app.add_url_rule('/asset-type/<int:asset_type_id>/create-asset', 'create-asset', create_asset, methods=['GET', 'POST'])
 
 if __name__ == '__main__':
--- src/server/asset_type_server.py
+++ src/server/asset_type_server.py
@@ -4,7 +4,6 @@
 
 These are the routes for the ``AssetTypeManager``.
 """
-
 from typing import List
 
 from flask import redirect, render_template, request
@@ -12,127 +11,148 @@ from flask import redirect, render_template, request
 from asset import AssetType
 from asset.asset_manager import AssetManager
 from asset.asset_type_manager import AssetTypeManager
-from database import Column, DataTypes
+from config import Config
+from database import Column, DataType, DataTypes
 from exceptions.asset import AssetTypeDoesNotExistException
+from exceptions.common import IllegalStateException
 from pages import AssetTypePage
 from plugins import Plugin, PluginSettings
 
 
-def create_asset_type():
-    """This is a FlaskAppRoute that lets you create ``AssetType`` dialog."""
+class AssetTypeServer:
+    """This is the AssetTypeServer."""
 
-    asset_type_manager = AssetTypeManager()
-    asset_types = {
-        asset_type.asset_name: asset_type.asset_type_id
-        for asset_type in asset_type_manager.get_all()
-    }
+    _instance = None
+    _json_response = None
 
-    if request.method == 'POST':
+    @staticmethod
+    def get():
+        """Get the instance of this singleton."""
+        if not AssetTypeServer._instance:
+            AssetTypeServer._instance = AssetTypeServer()
+        return AssetTypeServer._instance
 
-        asset_name = request.form.get('assetName')
+    def __init__(self):
+        """Create a new AssetTypeServer."""
+        self._json_response = bool(Config.get().read('frontend', 'json_response', False))
 
-        columns: List[Column] = []
-        for column_name, column_datatype, column_required in \
-                [(f'columnName_{i}', f'columnDataType_{i}', f'columnRequired_{i}') for i in range(0, 15)]:
-            if column_name in request.form.keys():
-
-                # Get the columns datatype from the form
-                datatype_str = request.form.get(column_datatype)
-
-                # Checking if the set data type is know to the system
-                if datatype_str in DataTypes.get_all_type_names():
-                    asset_type = 0
-                    datatype = DataTypes[datatype_str].value
-
-                # If not, the only possible valid entry into this field
-                # is another asset_type -> Check if it exists
-                elif asset_type_manager.get_one(int(datatype_str)):
-                    asset_type = int(datatype_str)
-                    datatype = DataTypes.INTEGER.value
-
-                # If the datatype is unknown an exception is raised
-                # this should never be the case, since the drop down
-                # menu of available data types is filled from the BE
-                else:
-                    raise AssetTypeDoesNotExistException("The data type you referenced does not exist!")
-
-                column_name = request.form.get(column_name)
-                column_db_name = column_name.replace(' ', '_').lower()
-
-                required = request.form.get(column_required) == 'checkboxTrue'
-
-                columns.append(Column(
-                    name=column_name,
-                    db_name=column_db_name,
-                    datatype=datatype,
-                    asset_type=asset_type,
-                    required=required
-                ))
-            else:
-                break
-
-        new_asset_type = AssetType(
-            asset_name=asset_name,
-            columns=columns
-        )
+    @staticmethod
+    def create_asset_type():
+        """This is a FlaskAppRoute that lets you create ``AssetType`` dialog."""
 
         asset_type_manager = AssetTypeManager()
-        asset_type_manager.create_asset_type(new_asset_type)
+        asset_types = {
+            asset_type.asset_name: asset_type.asset_type_id
+            for asset_type in asset_type_manager.get_all()
+        }
 
-        return redirect('/configuration')
+        # When this is called using POST, we create
+        # an asset type in the database.
+        if request.method == 'POST':
 
-    data_types = DataTypes.get_all_data_types()
+            columns: List[Column] = []
+            asset_name = request.form.get('assetName')
 
-    return render_template(
-        "create-asset-type.html",
-        data_types=data_types,
-        asset_types=asset_types
-    )
+            for column_number in range(0, 15):
 
+                column_name = f'column-name-{column_number}'
+                column_datatype = f'column-data-type-{column_number}'
+                column_required = f'column-required-{column_number}'
 
-def get_all_asset_types():
-    """This is a FlaskAppRoute that shows ``AssetTypes`` available using asset-types.html."""
+                if column_name in request.form.keys():
 
-    asset_type_manager = AssetTypeManager()
-    asset_types = asset_type_manager.get_all()
+                    # Get the columns datatype from the form
+                    datatype_str = request.form.get(column_datatype)
 
-    return render_template("asset-types.html", asset_types=asset_types)
+                    # Checking if the set data type is know to the system
+                    if datatype_str in DataTypes.get_all_type_names():
+                        asset_type = 0
+                        datatype = DataTypes[datatype_str].value
 
+                    # If the datatype is unknown an exception is raised
+                    # this should never be the case, since the drop down
+                    # menu of available data types is filled from the BE
+                    else:
+                        raise AssetTypeDoesNotExistException(
+                            "The data type you referenced does not exist!")
 
-def get_one_asset_type(asset_type_id):
-    """Show the Detail Page for an ``AssetType``."""
+                    column_name = request.form.get(column_name)
+                    column_db_name = column_name.replace(' ', '_').lower()
 
-    asset_type_manager = AssetTypeManager()
-    asset_type = asset_type_manager.get_one(asset_type_id)
+                    required = request.form.get(column_required) == 'checkboxTrue'
 
-    asset_manager = AssetManager()
-    assets = asset_manager.get_all(asset_type)
+                    columns.append(Column(
+                        name=column_name,
+                        db_name=column_db_name,
+                        datatype=datatype,
+                        asset_type=asset_type,
+                        required=required
+                    ))
+                else:
+                    break
 
-    asset_type_page = AssetTypePage(
-        asset_type=asset_type,
-        assets=assets,
-        number_of_fields=1,
-        layout_macro_path='layouts/one_one_layout.html',
-        plugin_settings=[
-            PluginSettings(
-                plugin=Plugin('list_assets', 'plugins/list_assets_plugin.html'),
-                employed_columns={
-                    0: 'Column 1',
-                    1: 'Column 2'
-                }
+            if not columns:
+                raise IllegalStateException("Can't create an asset type without any columns!")
+
+            new_asset_type = AssetType(
+                asset_name=asset_name,
+                columns=columns
             )
-        ]
-    )
 
-    return render_template("asset-type.html", asset_type_page=asset_type_page, assets=assets)
+            asset_type_manager = AssetTypeManager()
+            asset_type_manager.create_asset_type(new_asset_type)
 
+            return redirect('/configuration')
 
-def delete_asset_type(asset_type_id):
-    """Delete the ``AssetType`` with id ``asset_type_id``."""
+        data_types: List[DataType] = DataTypes.get_all_data_types()
 
-    asset_type_manager = AssetTypeManager()
-    asset_type = asset_type_manager.get_one(asset_type_id)
+        return render_template(
+            "create-asset-type.html",
+            data_types=data_types,
+            asset_types=asset_types
+        )
 
-    if request.form.get('deleteAsset') == 'True':
-        asset_type_manager.delete_asset_type(asset_type)
-        return redirect('/configuration')
+    @staticmethod
+    def get_all_asset_types():
+        """This is a FlaskAppRoute that shows ``AssetTypes`` available using asset-types.html."""
+
+        asset_type_manager = AssetTypeManager()
+        asset_types = asset_type_manager.get_all()
+
+        return render_template("asset-types.html", asset_types=asset_types)
+
+    @staticmethod
+    def get_one_asset_type(asset_type_id):
+        """Show the Detail Page for an ``AssetType``."""
+
+        asset_type_manager = AssetTypeManager()
+        asset_type = asset_type_manager.get_one(asset_type_id)
+
+        asset_manager = AssetManager()
+        assets = asset_manager.get_all(asset_type)
+
+        asset_type_page = AssetTypePage(
+            asset_type=asset_type,
+            assets=assets,
+            number_of_fields=1,
+            layout_macro_path='layouts/one_one_layout.html',
+            plugin_settings=[
+                PluginSettings(
+                    plugin=Plugin('list_assets', 'plugins/list_assets_plugin.html'),
+                    employed_columns=['Column 1', 'Column 2']
+                )
+            ]
+        )
+
+        return render_template("asset-type.html", asset_type_page=asset_type_page, assets=assets)
+
+    @staticmethod
+    def delete_asset_type(asset_type_id):
+        """Delete the ``AssetType`` with id ``asset_type_id``."""
+
+        asset_type_manager = AssetTypeManager()
+        asset_type = asset_type_manager.get_one(asset_type_id)
+
+        if request.form.get('deleteAsset') == 'True':
+            asset_type_manager.delete_asset_type(asset_type)
+            return redirect('/configuration')
