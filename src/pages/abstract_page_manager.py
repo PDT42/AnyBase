baee7680b1d416552a5f6732e7e00888a384820d
PDT <darkflayer@gmail.com>
2020-08-14 15:12:43 +0200

    updated abstract page manager, updated row conversion functions

--- src/pages/abstract_page_manager.py
+++ src/pages/abstract_page_manager.py
@@ -9,10 +9,11 @@ from abc import abstractmethod
 from re import findall
 from typing import Any, List, Mapping, Optional
 
-from asset import AssetType
+from asset import Asset, AssetType
+from asset.asset_manager import AssetManager
+from asset.asset_type_manager import AssetTypeManager
 from database.db_connection import DbConnection
-from exceptions.plugins import ItemNotFoundException
-from pages import AssetDetailPage, AssetTypePage, ColumnInfo, PageLayout, RowInfo
+from pages import ColumnInfo, PageLayout
 
 
 class APageManager:
@@ -20,91 +21,115 @@ class APageManager:
 
     # Variables filled by the implementation
     db_connection: DbConnection = None
+    asset_type_manager: AssetTypeManager = None
+    asset_manager: AssetManager = None
+
     asset_type_layout_table_name: str = None
     asset_type_plugin_table_name: str = None
 
     @abstractmethod
-    def create_page(self, asset_page: AssetTypePage):
-        """Create a new ``AssetPage`` in the database."""
+    def create_page(self, page_layout: PageLayout):
+        """Create a new ``PageLayout`` in the database."""
         pass
 
     @abstractmethod
-    def delete_page(self, asset_type: AssetType):
+    def delete_page(self, page_layout: PageLayout):
         """Delete the ``AssetPage`` of a given ``asset_type_id``."""
         pass
 
     @abstractmethod
-    def update_page(self, asset_page: AssetTypePage):
+    def update_page(self, page_layout: PageLayout):
         """Update an ``AssetPage`` in the database."""
         pass
 
     @abstractmethod
-    def check_page_exists(self, asset_type: AssetType):
+    def check_page_exists(self, page_layout: PageLayout):
         """Check if an ``AssetPage`` for ``asset_type_id`` exists in the database."""
         pass
 
     @abstractmethod
-    def get_page(self, asset_type: AssetType):
+    def get_page(self, asset_type: AssetType, asset: Asset):
         """Get the ``AssetPage`` for ``asset_type_id`` from the database"""
         pass
 
     @abstractmethod
-    def get_plugin(self, column_id: int):
+    def get_column_info(self, column_id: int):
         """Get the plugin with ``column_id``. """
         pass
 
     @abstractmethod
-    def get_editor(self, asset_type: AssetType):
+    def get_editor(self, page_layout: PageLayout):
         """Get the editor for a given ``asset_type_id``."""
         pass
 
     @staticmethod
-    def convert_layout_to_row(page: PageLayout):
+    def convert_layout_to_row(page_layout: PageLayout) -> Mapping[str, Any]:
         """Convert a PageLayout Type to a row."""
 
-        item_id: Optional[int] = None
-
-        if isinstance(page, AssetTypePage):
-            item_id = page.asset_type.asset_type_id
-        elif isinstance(page, AssetDetailPage):
-            item_id = page.asset.asset_id
-
-        if not item_id:
-            raise ItemNotFoundException("The item is unknown.")
-
         # Constructing the layout string
-        layout: str = "["
+        layout_str: str = "["
 
-        for row_info in page.layout:
+        for row_info in page_layout.layout:
             columns = ";".join([
                 f"{col.column_width}, {col.column_id}"
-                for col in row_info.columns
+                for col in row_info
             ])
-            layout += "{" + str(columns) + "}"
-        layout += "]"
+            layout_str += "{" + str(columns) + "}"
+        layout_str += "]"
+
+        asset_id = None
+        if isinstance(page_layout.asset, Asset):
+            asset_id = page_layout.asset.asset_id
 
         # Creating a dict as required by db query
         layout_row: Mapping[str, Any] = {
-            'item_id': page.asset_type.asset_type_id,
-            'layout': str(layout)
+            'primary_key': page_layout.layout_id,
+            'items_url': page_layout.items_url,
+            'asset_id': asset_id,
+            'asset_type_id': page_layout.asset_type.asset_type_id,
+            'layout': str(layout_str)
         }
         return layout_row
 
-    def convert_row_data_to_layout(self, row_data: str):
+    def convert_row_data_to_layout(self, row_data: Mapping[str, Any]):
         """Convert a row to a PageLayout."""
 
-        layout: List[RowInfo] = []
+        layout: List[List[ColumnInfo]] = []
 
-        for row in findall("{([^}]*)}", row_data):
+        # Using the create syntax used in convert_layout_to_row
+        # to extract ColumnInfo objects from db row.
 
-            row_info: RowInfo = RowInfo(columns=[])
+        # row_data: "[{column_width, column_id; ...}, ...]"
 
-            for col in row.split(';'):
-                column_width, column_id = col.replace('(', '') \
-                    .replace(')', '').split(', ')
+        # row: "{column_width, column_id; ...}"
+        for row in findall("{([^}]*)}", row_data['layout']):
 
-                row_info.columns.append(self.get_plugin(int(column_id)))
+            row_info: List[ColumnInfo] = []
+
+            # column: "column_width, column_id"
+            for column in row.split(';'):
+                column_width, column_id = column.split(', ')
+                row_info.append(self.get_column_info(int(column_id)))
 
             layout.append(row_info)
 
-        return layout
+        # Getting the asset type using the manager
+        # filled by the child of this superclass.
+
+        asset_type: AssetType = self.asset_type_manager \
+            .get_one(row_data['asset_type_id'])
+
+        asset: Optional[Asset] = None
+
+        if asset_id := row_data['asset_id']:
+            asset = self.asset_manager.get_one(asset_id, asset_type)
+
+        # Finally returning a PageLayout
+
+        return PageLayout(
+            layout=layout,
+            asset_type=asset_type,
+            items_url=row_data['items_url'],
+            asset=asset,
+            layout_id=row_data['primary_key']
+        )
