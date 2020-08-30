29ee934e5c7e82a9cdf84347f8db61d319e55212
PDT <darkflayer@gmail.com>
2020-08-30 20:32:52 +0200

    updated tests for pages

--- src/test/test_page_manager.py
+++ src/test/test_page_manager.py
@@ -12,9 +12,10 @@ from asset import Asset, AssetType
 from asset.asset_manager import AssetManager
 from asset.asset_type_manager import AssetTypeManager
 from database import Column, DataTypes
-from pages import AssetPageLayout, ColumnInfo, PageLayout
+from pages import ColumnInfo, PageLayout
 from pages.abstract_page_manager import APageManager
 from pages.page_manager import PageManager
+from plugins import PluginRegister
 from test.test_util import init_test_db
 
 
@@ -43,53 +44,54 @@ class TestPageManager(TestCase):
         self.asset_manager.create_asset(self.asset_type, self.asset)
         self.asset = self.asset_manager.get_one(1, self.asset_type)
 
-        # Defining a Page Layout with asset = None
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Defining a Page Layout with asset_type_page = False
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         self.asset_type_layout: PageLayout = PageLayout(
-            asset_type=self.asset_type,
-            items_url=f'/asset-type:{self.asset_type.asset_type_id}/items',
-            layout=[[
+            asset_type_id=self.asset_type.asset_type_id,
+            asset_page_layout=False,
+            field_mappings={
+                'header': 'name'
+            }, layout=[[
                 ColumnInfo(
-                    plugin_name='list-assets',
-                    plugin_path='plugins/list-assets-plugin.html',
+                    plugin=PluginRegister.LIST_ASSETS.value,
                     field_mappings={'header': 'name'},
                     column_width=12,
                     column_id=0
                 )]
-            ]
-        )
+            ])
+
         self.asset_type_layout_row: Mapping[str, Any] = {
-            'primary_key': None,
-            'items_url': '/asset-type:1/items',
-            'asset_id': None,
             'asset_type_id': 1,
-            'layout': '[{12, 0}]'
+            'asset_page_layout': 0,
+            'layout': '{12, 0}',
+            'field_mappings': "{'header': 'name'}",
+            'primary_key': None
         }
 
-        # Defining a Page Layout with asset = self.asset
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Defining a Page Layout with asset_type_page = True
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-        self.asset_layout: AssetPageLayout = AssetPageLayout(
-            asset_type=self.asset_type,
-            items_url=f'/asset-type:{self.asset_type.asset_type_id}/items',
-            asset=self.asset.asset_id,
-            layout=[[
+        self.asset_layout: PageLayout = PageLayout(
+            asset_type_id=self.asset_type.asset_type_id,
+            asset_page_layout=True,
+            field_mappings={
+                'header': 'name'
+            }, layout=[[
                 ColumnInfo(
-                    plugin_name='list-assets',
-                    plugin_path='plugins/list-assets-plugin.html',
+                    plugin=PluginRegister.ASSET_DETAILS.value,
                     column_width=12,
                     field_mappings={'header': 'name'},
-                    column_id=0
+                    column_id=1
                 )]
-            ]
-        )
+            ])
+
         self.asset_layout_row: Mapping[str, Any] = {
-            'primary_key': None,
-            'items_url': '/asset-type:1/items',
-            'asset_id': None,
             'asset_type_id': 1,
-            'layout': '[{12, 0}]'
+            'asset_page_layout': 1,
+            'layout': '{12, 1}',
+            'field_mappings': "{'header': 'name'}",
+            'primary_key': None
         }
 
     def tearDown(self) -> None:
@@ -100,18 +102,16 @@ class TestPageManager(TestCase):
     def test_convert_layout_to_row(self):
         """Test superclass method ``convert_layout_to_row``."""
 
-        self.assertEqual(
-            self.page_manager.convert_layout_to_row(self.asset_type_layout),
-            self.asset_type_layout_row
-        )
-        self.assertEqual(
-            self.page_manager.convert_layout_to_row(self.asset_layout),
-            self.asset_layout_row
-        )
+        asset_type_layout_row = self.page_manager.convert_layout_to_row(self.asset_type_layout)
+        self.assertEqual(asset_type_layout_row, self.asset_type_layout_row)
+
+        asset_layout_row = self.page_manager.convert_layout_to_row(self.asset_layout)
+        self.assertEqual(asset_layout_row, self.asset_layout_row)
 
     def test_create_page_get_page(self):
         """Test Create Page."""
 
-        self.asset_type_layout.layout_id = self.page_manager.create_page(self.asset_type_layout)
-        self.assertEqual(self.page_manager.get_page(self.asset_type), self.asset_type_layout)
+        created_asset_type_layout = self.page_manager.create_page(self.asset_type_layout)
+        gotten_asset_type_layout = self.page_manager.get_page(self.asset_type.asset_type_id, False)
 
+        self.assertEqual(gotten_asset_type_layout, created_asset_type_layout)
