6dfadad599b7c5b2ad30205d605d3933603eb5c9
PDT <darkflayer@gmail.com>
2020-09-16 01:43:49 +0200

    updated test_util

--- src/test/test_util.py
+++ src/test/test_util.py
@@ -7,6 +7,7 @@ These are utility functions for testing.
 from tempfile import mkdtemp
 from typing import Tuple
 
+from config import Config
 from database.db_connection import DbConnection
 from database.sqlite_connection import SqliteConnection
 
@@ -15,5 +16,6 @@ def init_test_db() -> Tuple[str, DbConnection]:
     """Create a new DbConnection singleton connected to a special
     test database."""
 
+    Config.get().change_path('U:/projects/anybase_modular_management/res/config.ini')
     tempdir = mkdtemp()
     return tempdir, SqliteConnection.get(f"{tempdir}\\database.db")
