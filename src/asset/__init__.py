0f50c4bce5aa3811877d74930601a5f8692b88a1
PDT <darkflayer@gmail.com>
2020-09-16 01:44:29 +0200

    Moved the AssetType dataclass to the asset_type package

--- src/asset/__init__.py
+++ src/asset/__init__.py
@@ -2,46 +2,13 @@
 :Author: PDT
 :Since: 2020/05/28
 
-The package contains the AssetManager and the AssetTypeManager.
+The package contains everything ``Asset`` related.
 """
+
 from dataclasses import dataclass
 from datetime import date, datetime, time
-from typing import Any, List, MutableMapping, Optional, Union
+from typing import Any, MutableMapping, Optional
 from uuid import uuid4
-
-from database import Column
-
-
-@dataclass
-class AssetType:
-    """This is a ``AssetType``, it defines an asset."""
-
-    asset_name: str
-    columns: List[Column]
-    created: datetime = None
-    updated: datetime = None
-    asset_table_name: str = None
-    asset_type_id: int = None
-    super_type: Union['AssetType', int] = 0
-    owner_id: int = 0
-
-    def as_dict(self):
-        return {
-            'asset_name': self.asset_name,
-            'columns': [col.as_dict() for col in self.columns],
-            'created': int(self.created.timestamp()),
-            'updated': int(self.created.timestamp()),
-            'asset_table_name': self.asset_table_name,
-            'asset_type_id': self.asset_type_id,
-            'super_type': self.super_type,
-            'owner_id': self.owner_id
-        }
-
-    def get_super_type_id(self) -> int:
-        if isinstance(self.super_type, int):
-            return self.super_type
-        elif isinstance(self.super_type, AssetType):
-            return self.super_type.asset_type_id
 
 
 @dataclass
