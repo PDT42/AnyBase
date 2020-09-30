03b2a8414b85e63a959006ea3f16c3e2f9abc3d9
PDT <darkflayer@gmail.com>
2020-09-30 23:03:56 +0200

    added app feature concept

---
+++ src/app_features/__init__.py
@@ -0,0 +1,11 @@
+"""
+:Author: PDT
+:Since: 2020/09/26
+
+This is the package for 'AppFeatures'. An AppFeature is basically
+a set of functionalities, serving the application context instead
+of a per Asset/Type context.
+
+NOTE: Eg. an AppFeature could be an exporter or graph renderer.
+"""
+
