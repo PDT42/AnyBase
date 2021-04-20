3ca1a2a65a063ef079cc0bd553bc09b8ae04ce45
PDT <darkflayer@gmail.com>
2021-04-21 00:10:18 +0200

    updated anybute, added required field

--- anybase-backend/src/anybute/anybute.entity.ts
+++ anybase-backend/src/anybute/anybute.entity.ts
@@ -1,5 +1,5 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { MetaAnyty } from '../meta-anyty/meta-anyty.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { MetaAnyty } from "../meta-anyty/meta-anyty.entity";
 
 const SQLITE_REGEX = /([ &])/g;
 
@@ -20,24 +20,28 @@ export class Anybute {
   @Column({ nullable: false })
   dataType: string;
 
+  @Column({ nullable: false })
+  required: boolean;
+
   @ManyToOne(() => MetaAnyty, (metaAnyty) => metaAnyty.anybutes)
   metaAnyty: MetaAnyty;
 }
 
 export interface AnybuteDTO {
-  columnName: string;
+  nameRep: string;
   dataType: string;
+  required: boolean;
 }
 
 export function createAnybute(
   columnName: string,
   nameRep: string,
   dataType: string,
-  metaAnyty: MetaAnyty,
+  metaAnyty: MetaAnyty
 ): Anybute {
   const newAnybute = new Anybute();
 
-  newAnybute.columnName = columnName.replace(SQLITE_REGEX, '').toLowerCase();
+  newAnybute.columnName = columnName.replace(SQLITE_REGEX, "").toLowerCase();
   newAnybute.nameRep = nameRep;
   newAnybute.dataType = dataType;
   newAnybute.metaAnyty = metaAnyty;
