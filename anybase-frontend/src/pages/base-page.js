974fe9ec7d7a5a8b0b04e8c684c6a31dc508d64e
PDT <darkflayer@gmail.com>
2021-04-21 00:15:06 +0200

    added navigation to details and create pages

--- anybase-frontend/src/pages/base-page.js
+++ anybase-frontend/src/pages/base-page.js
@@ -4,7 +4,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import styles from "../components/common/common-styles.module.css";
 
 import { Navigation } from "../components/navigation/navigation";
+import { AnytyEditor } from "./anyty-editor";
 import { Manyties } from "./manyties";
+import { MAnytyDetails } from "./manyty-details";
 import { MAnytyEditor } from "./manyty-editor"
 import { Users } from "./users";
 
@@ -20,6 +22,8 @@ export function BasePage() {
 
           <Route exact path="/manyties" component={Manyties} />
           <Route exact path="/manyties/create" component={MAnytyEditor} />
+          <Route path="/manyties/:manyty_id/create/" component={AnytyEditor} />
+          <Route path="/manyties/:manyty_id/details" component={MAnytyDetails} />
           <Route exact path="/users" component={Users} />
 
         </div>
