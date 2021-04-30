7d125ded839ba99abf263a4643f1e0b1a064df61
PDT <darkflayer@gmail.com>
2021-04-30 12:18:36 +0200

    started implementing meta anyty dashboard view

--- anybase-frontend/src/pages/manyties.js
+++ anybase-frontend/src/pages/manyties.js
@@ -1,39 +1,102 @@
 import React from "react";
-import { Button } from "@material-ui/core";
+import { Box, Button, Grid, Paper } from "@material-ui/core";
 
-import { TitleRowButton } from "../components/common/title-row";
-import { AnytyList } from "../components/item-list/item-list";
+import { TitleRowButtons } from "../components/common/title-row";
 import { getAllMAnyties } from "../services/meta-anyty/meta-anyty-service";
 
+import styles from "../components/common/common-styles.module.css";
+
 export class Manyties extends React.Component {
+
   constructor(props) {
     super(props);
+
     this.state = {
-      listAnyties: []
+      metaAnyties: []
     };
   }
 
   componentDidMount() {
+
     getAllMAnyties().then((mAnyties) => {
-      this.setState({ listAnyties: mAnyties })
+      this.setState({ metaAnyties: mAnyties })
     }).catch(r => {
       console.log(r);
-      this.setState({ listAnyties: [] });
+      this.setState({ metaAnyties: [] });
     });
   }
 
   render() {
     return (
-      <div>
-        <TitleRowButton titleText="Meta Anyties" button={
-          <Button variant="contained"
-            color="secondary"
-            size="medium"
-            onClick={() => this.props.history.push('/manyties/create')}
-          >add</Button>
-        } />
-        <AnytyList {...this.props} listAnyties={this.state.listAnyties} />
-      </div>
+      <Grid container direction="column" spacing={2}>
+        <Grid item container>
+          <TitleRowButtons titleText="Meta Anyties" buttons={[
+            <Button variant="contained"
+              color="secondary"
+              size="medium"
+              onClick={() => this.props.history.push('/manyties/create')}
+            >ADD</Button>
+          ]} />
+        </Grid>
+
+        <Grid item>
+          <MAnytyList {...this.props} metaAnyties={this.state.metaAnyties} />
+        </Grid>
+
+      </Grid>
+    );
+  }
+}
+
+class MAnytyList extends React.Component {
+
+  onListItemNavigate = (mAnytyId) => {
+    this.props.history.push(`/manyties/${mAnytyId}/details`);
+  };
+
+  render() {
+    if (!this.props.metaAnyties) {
+      return (
+        <span>Loading ...</span>
+      );
+    }
+
+    // Create a list item for each of the MAnyties
+    const mAnytyList = this.props.metaAnyties.map((mAnyty, index) => {
+      return (
+        <MAnytyListItem key={mAnyty._manyty_id} mAnyty={mAnyty} mAnytyIndex={index}
+          onClick={() => this.onListItemNavigate(mAnyty._manyty_id)}
+        />
+      );
+    });
+
+    return (
+      <Grid container direction="column" spacing={2}>
+        {mAnytyList}
+      </Grid>
+    );
+  }
+}
+
+class MAnytyListItem extends React.Component {
+  render() {
+    return (
+      <Grid item xs={12}>
+        <Paper className={styles.paperListItem}>
+          <Box p={1} onClick={this.props.onClick}>
+            <Grid container direction="column" spacing={1}>
+              <Grid item container direction="row" justify="space-between">
+                <Grid item>
+                  <span>{this.props.mAnyty.nameRep}</span>
+                </Grid>
+                <Grid item>
+                  <span>{this.props.mAnyty._manyty_id}</span>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Box>
+        </Paper>
+      </Grid>
     );
   }
 }
