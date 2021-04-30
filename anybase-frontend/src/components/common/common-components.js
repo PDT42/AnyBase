df8c62816752937cfabe5928242f0d6b0c3e2bad
PDT <darkflayer@gmail.com>
2021-04-30 12:22:44 +0200

    removed ract javascript app

--- anybase-frontend/src/components/common/common-components.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import styles from "./common-styles.module.css";
-
-export class HDivider extends React.Component {
-    render() {
-        return <div className={styles.hDivider} />
-    };
-}
