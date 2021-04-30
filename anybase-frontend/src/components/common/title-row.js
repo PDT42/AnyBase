86166138985ddb84d54956476eeb0d34345ce262
PDT <darkflayer@gmail.com>
2021-04-30 12:18:06 +0200

    using Grid instead of custom style

--- anybase-frontend/src/components/common/title-row.js
+++ anybase-frontend/src/components/common/title-row.js
@@ -1,4 +1,6 @@
-import { HDivider } from "../common/common-components"
+import { Grid } from "@material-ui/core";
+import React from "react";
+import { HDivider } from "./common-components"
 
 import styles from "./common-styles.module.css"
 
@@ -14,14 +16,26 @@ export function TitleRow({ titleText }) {
     );
 }
 
-export function TitleRowButton({ titleText, button }) {
-    return (
-        <div>
-            <div className={styles.titleRow}>
-                <span className={styles.titleText}>{titleText}</span>
-                <div>{button}</div>
-            </div>
-            <HDivider />
-        </div>
-    );
+export class TitleRowButtons extends React.Component {
+    render() {
+        const buttons = this.props.buttons.map((button, index) => {
+            return (<Grid item key={index}>{button}</Grid>);
+        })
+
+        return (
+            <Grid container direction="column" spacing={1}>
+                <Grid item container direction="row" alignItems="center">
+                    <Grid item xs={8}>
+                        <span className={styles.titleText}>{this.props.titleText}</span>
+                    </Grid>
+                    <Grid item xs={4} container direction="row" spacing={1} justify="flex-end">
+                        {buttons}
+                    </Grid>
+                </Grid>
+                <Grid item>
+                    <HDivider />
+                </Grid>
+            </Grid>
+        );
+    }
 }
